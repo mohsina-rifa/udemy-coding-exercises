b424273e77ddcb94d5b57282dfa647b79f20dc3e
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2026-04-29 15:07:47 +0600

    Exercise-02: Array Methods

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 /*
-Your task is to write a new function that should be named combine and have the following characteristics:
-  ⮞ Accept three input values
-  ⮞ Calculate a new value based on the three input values: a * b / c (if a, b & c are the input values)
-  ⮞ Return the calculated result
+Your task is to add the missing logic to a transformToObjects() function that should transform a list of numbers into a list of JavaScript objects.
+
+In the newly returned array, every object must have a val key and the input array's number as a value.
+
+For example, for the provided input [1, 2, 3] the transformToObjects([1, 2, 3]) function should return [{val: 1}, {val: 2}, {val: 3}].
 */
 
 
-function combine(a, b, c) {
-    return (a * b / c);
+
+function transformToObjects(numberArray) {
+    return numberArray.map(num => {
+        return { val: num };
+    });
 }
