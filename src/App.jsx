80a05d464a562a277f03743c6238c947c4bbfd3e
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2026-04-29 15:44:06 +0600

    Exercise-11: Dynamic Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,25 @@
 /*
-You're working on a part of a web app that's responsible for showing a warning when a user is about to perform a dangerous action.
+Your task is to dynamically apply a CSS class (active) to the <p>Style me</p> element in the provided React app.
 
-Therefore, your task is to conditionally show a warning box once a user has clicked a specific button. Inside that warning dialog, another button allows users to dismiss the warning (i.e., remove the warning box from the screen).
-
-For this task, you must react to clicks on both <button> elements that are part of the starting code. The second button, outside of the <div> with the id="alert", should show the <div id="alert"> (and all its content). The button inside that <div> should then hide it again (i.e., remove it from the DOM).
-
-It's up to you whether you want to use a ternary expression or store the conditionally shown JSX code in a variable.
+The class should be applied when the <button> is clicked for the first time.
 */
 
 
 
 import React from 'react';
 
-// IMPORTANT:
-// In this Udemy environment, you CAN'T import & use useState like this:
-// import { useState } from 'react'
-// Instead, import & use it like this:
-// import React from 'react';
-// React.useState(...)
-
 // don't change the Component name "App"
 export default function App() {
-  const [showAlert, setShowAlert] = React.useState(false);
+  const [isActive, setIsActive] = React.useState(false);
 
-  function handleDeleteClick() {
-    setShowAlert(true);
-  }
-
-  function handleProceedClick() {
-    setShowAlert(false);
+  function handleToggle() {
+    setIsActive((prev) => !prev);
   }
 
   return (
     <div>
-      {showAlert ? (
-        <div data-testid="alert" id="alert">
-          <h2>Are you sure?</h2>
-          <p>These changes can't be reverted!</p>
-          <button onClick={handleProceedClick}>Proceed</button>
-        </div>
-      ) : (
-        <button onClick={handleDeleteClick}>Delete</button>
-      )}
+      <p className={isActive ? 'active' : ''}>Style me!</p>
+      <button onClick={handleToggle}>Toggle style</button>
     </div>
   );
 }
