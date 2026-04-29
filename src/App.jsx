0ce01f4b19a317e5585d4cf987ed70688d874ac9
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2026-04-29 15:51:26 +0600

    Exercise-12: Dynamic List Content

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 /*
-Your task is to dynamically apply a CSS class (active) to the <p>Style me</p> element in the provided React app.
+You're working on a "Todo List" web app and your task is to output a list of dummy todo items dynamically. For this task, a Todo component has been prepared for you, though you must still add some code to it to receive and output the todo text.
 
-The class should be applied when the <button> is clicked for the first time.
+To be more precise: In the App component, you should transform the DUMMY_TODOS array that's provided to you (which must not be changed!) to a list of JSX elements (<Todo> elements to be precise). Every Todo component item must receive and output the todo text via a prop called text.
 */
 
 
 
 import React from 'react';
 
+import Todo from './Todo';
+
+// don't remove the export keyword here!
+export const DUMMY_TODOS = [
+    'Learn React',
+    'Practice React',
+    'Profit!'
+];
+
 // don't change the Component name "App"
 export default function App() {
-  const [isActive, setIsActive] = React.useState(false);
-
-  function handleToggle() {
-    setIsActive((prev) => !prev);
-  }
-
   return (
     <div>
-      <p className={isActive ? 'active' : ''}>Style me!</p>
-      <button onClick={handleToggle}>Toggle style</button>
+      {DUMMY_TODOS.map((todo, index) => (
+        <Todo key={index} text={todo} />
+      ))}
     </div>
   );
 }
